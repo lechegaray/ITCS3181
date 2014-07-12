b3d80ae5475d00c7a4a2a849e673ef0b5eaf2040
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-12 15:07:56 -0400

    completed 4-bit adder

---
+++ 4-bitAdder.cmd
@@ -0,0 +1,12 @@
+restart
+wave 4-bitadder.wfm A0 A1 A2 A3 B0 B1 B2 B3 CIN S0 S1 S2 S3 COUT
+pattern A0 1 1 1 0 0 0 0 0 1
+pattern A1 0 1 1 0 1 1 1 0 1
+pattern A2 0 0 0 0 1 0 1 0 0
+pattern A3 0 1 0 0 1 0 1 0 1
+pattern B0 0 1 1 0 1 0 1 0 1
+pattern B1 0 0 0 0 1 0 1 0 1
+pattern B2 0 1 0 1 1 0 1 0 0
+pattern B3 0 0 1 0 1 0 1 0 1
+pattern CIN 0 1 0 0 1 0 1 0 1
+run
