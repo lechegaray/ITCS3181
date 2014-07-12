29e449d5df3728f78b7ec53dd30cdde770e764c7
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-12 13:24:13 -0400

    2nd commit to add c-and21

--- c-and21.cmd
+++ c-and21.cmd
@@ -1,11 +1,5 @@
 restart
-wave complement.wfm A0 B0 A1 B1 A2 B2 A3 B3 Q0 Q1 Q2 Q3
-pattern A0 0 0 1 1 
-pattern B0 0 1 0 1
-pattern A1
-pattern B1
-pattern A2
-pattern B2
-pattern A3
-pattern B3 
+wave complement.wfm A B Q
+pattern A 0 0 1 1 
+pattern B 0 1 0 1
 run
