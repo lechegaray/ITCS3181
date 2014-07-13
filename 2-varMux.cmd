6fab39dffde66686a1316a787383b88f6f351775
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-13 10:24:27 -0400

    completed 2-var mux (made it out of three 1 var mux this time)

--- 2-varMux.cmd
+++ 2-varMux.cmd
@@ -1,9 +1,9 @@
 restart
-wave 2-varMux.wfm I0 I1 I2 I3 S0 S1 OUT
-pattern I0 1 0 0 0 
-pattern I1 0 1 0 0 
-pattern I2 0 0 1 0
-pattern I3 0 0 0 1
+wave 2-varMux.wfm D0 D1 D2 D3 S0 S1 OUT
+pattern D0 1 0 0 0 
+pattern D1 0 1 0 0 
+pattern D2 0 0 1 0
+pattern D3 0 0 0 1
 pattern S0 0 0 1 1
 pattern S1 0 1 0 1
 run
