76443f8c9f76cc5974d9dcf2490cf5e6b2560ab8
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-13 10:58:14 -0400

    finised 4 input expandable shifter

--- 4bitShifter.cmd
+++ 4bitShifter.cmd
@@ -1,8 +1,8 @@
 restart
-wave 4bitShifter.wfm D0 D1 D2 D3 CIN S0 S1 S2 S3
+wave 4-inputExpandableShifter.wfm D0 D1 D2 D3 C S0 S1 S2 S3
 pattern D0 1 0 0 0 1
 pattern D1 0 1 0 0 1
 pattern D2 0 0 1 0 0
 pattern D3 0 0 0 1 0
-pattern CIN 1 1 1 1 1
+pattern C0 1 1 1 1 1
 run
