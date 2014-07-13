2deadc1f79fe8b3bd7cf4f9ee6a93762df478f6b
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-13 16:34:25 -0400

    partially finished report 2 (just analysis left)

--- 4bitShifter.cmd
+++ 4bitShifter.cmd
@@ -1,8 +1,8 @@
 restart
-wave 4-inputExpandableShifter.wfm D0 D1 D2 D3 C S0 S1 S2 S3
-pattern D0 1 0 0 0 1
-pattern D1 0 1 0 0 1
-pattern D2 0 0 1 0 0
-pattern D3 0 0 0 1 0
-pattern C0 1 1 1 1 1
+wave 4-inputExpandableShifter.wfm D0 D1 D2 D3 C0 S0 S1 S2 S3
+pattern D0 1 0 0 0 1 1 0 0 0 1 1 0 0 1 1 0
+pattern D1 0 1 0 0 1 0 1 0 0 1 1 0 0 1 1 0
+pattern D2 0 0 1 0 0 0 0 1 0 0 1 0 1 1 1 0 
+pattern D3 0 0 0 1 0 0 0 0 1 0 1 0 1 1 1 0 
+pattern C0 1 1 1 1 1 0 0 0 0 0 1 0 1 1 0 1
 run
