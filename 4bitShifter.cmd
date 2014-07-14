8276072008660803804be94a64c1ea942f28bcde
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-14 13:04:38 -0400

    LAB REPORT 2 DONE

--- 4bitShifter.cmd
+++ 4bitShifter.cmd
@@ -1,8 +1,11 @@
 restart
-wave 4-inputExpandableShifter.wfm D0 D1 D2 D3 C0 S0 S1 S2 S3
-pattern D0 1 0 0 0 1 1 0 0 0 1 1 0 0 1 1 0
-pattern D1 0 1 0 0 1 0 1 0 0 1 1 0 0 1 1 0
-pattern D2 0 0 1 0 0 0 0 1 0 0 1 0 1 1 1 0 
-pattern D3 0 0 0 1 0 0 0 0 1 0 1 0 1 1 1 0 
-pattern C0 1 1 1 1 1 0 0 0 0 0 1 0 1 1 0 1
+wave 4-inputExpandableShifter.wfm D0 D1 D2 D3 S0 S1 SLIN SRIN Q0 Q1 Q2 Q3 SLOUT SROUT
+pattern D0 1 1 1 1
+pattern D1 0 0 1 1
+pattern D2 0 0 0 0
+pattern D3 1 1 0 0  
+pattern S0 0 1 0 1
+pattern S1 1 0 0 1
+pattern SLIN 1 1 0 0
+pattern SRIN 1 1 1 1
 run
