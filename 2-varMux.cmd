1fd8a1013eedd350e1d17b861124e9dc00d14acb
Luis Echegaray <mrluisechegaray@gmail.com>
2014-07-14 10:53:37 -0400

    fixed the god damn 2-variable mux (significance)

--- 2-varMux.cmd
+++ 2-varMux.cmd
@@ -1,9 +1,9 @@
 restart
 wave 2-varMux.wfm D0 D1 D2 D3 S0 S1 OUT
-pattern D0 1 0 0 0 
-pattern D1 0 1 0 0 
-pattern D2 0 0 1 0
-pattern D3 0 0 0 1
-pattern S0 0 0 1 1
-pattern S1 0 1 0 1
+pattern D0 1
+pattern D1 0 
+pattern D2 0
+pattern D3 0
+pattern S0 0
+pattern S1 0
 run
